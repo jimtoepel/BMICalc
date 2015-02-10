8d4123c692c57482d997b4710310eac8be735897
jimtoepel <jim.toepel@gmail.com>
2015-02-09 19:51:50 -0800

    set up initial struct

--- BMICalc/main.c
+++ BMICalc/main.c
@@ -8,8 +8,31 @@
 
 #include <stdio.h>
 
+
+// this program creates a STRUCT to hold multiple piece of data in a single "variable"
+
+struct Person {
+    float   heightInMeters;
+    int     weightInKilos;
+    
+};
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+
+    struct Person mikey;
+    mikey.heightInMeters = 1.7;
+    mikey.weightInKilos = 96;
+    
+    struct Person aaron;
+    aaron.heightInMeters = 1.97;
+    aaron.weightInKilos = 84;
+    
+    printf("mikey is %.2f meters tall\n", mikey.heightInMeters);
+    printf("mikey weighs %d kilograms\n", mikey.weightInKilos);
+    printf("aaron is %.2f meters tall\n", aaron.heightInMeters);
+    printf("aaron weighs %d kilograms\n", aaron.weightInKilos);
+    
+    
+    
     return 0;
 }
