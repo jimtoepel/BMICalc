674af2c00e68ddebc5ffbc3287d61d24cd8ac8d7
jimtoepel <jim.toepel@gmail.com>
2015-03-11 21:00:24 -0700

    Class Extensions
    Keeping’ those header files clean, bro.

--- BMICalc/main.c
+++ BMICalc/main.c
@@ -8,22 +8,27 @@
 
 #include <stdio.h>
 
+// here is the declaration of type Person
 
-// this program creates a STRUCT to hold multiple piece of data in a single "variable"
+typedef struct {
+    float heightInMeters;
+    int weightInKilos;
+} Person;
 
-struct Person {
-    float   heightInMeters;
-    int     weightInKilos;
-    
-};
+
+float bodyMassIndex(Person p)
+{
+    return p.weightInKilos / (p.heightInMeters * p.heightInMeters);
+}
+
 
 int main(int argc, const char * argv[]) {
 
-    struct Person mikey;
+    Person mikey;
     mikey.heightInMeters = 1.7;
     mikey.weightInKilos = 96;
     
-    struct Person aaron;
+    Person aaron;
     aaron.heightInMeters = 1.97;
     aaron.weightInKilos = 84;
     
@@ -32,6 +37,12 @@ int main(int argc, const char * argv[]) {
     printf("aaron is %.2f meters tall\n", aaron.heightInMeters);
     printf("aaron weighs %d kilograms\n", aaron.weightInKilos);
     
+    float bmi;
+    bmi = bodyMassIndex(mikey);
+    printf("mikey has a BMI or %.2f\n", bmi);
+    
+    bmi = bodyMassIndex(aaron);
+    printf("aaron has a BMI of %.2f\n", bmi);
     
     
     return 0;
